29cf2565f8ae7cdf342b0db802d30ce6a9885170
b-left-code-c <UseOut@outlook.fr>
2026-05-04 15:18:29 +0000

    Fix xstrstr function prototype in xstring.h

--- xstring.h
+++ xstring.h
@@ -13,6 +13,6 @@ extern char *xstrchr(char *chaine, char c);
 
 extern char *xstrpbrk(char *chaine_1, char *chaine_2);
 
-extern const char *xstrstr(char *chaine_1, char *chaine_2);
+extern const char *xstrstr(const char *chaine_1, const char *chaine_2);
 
 #endif
